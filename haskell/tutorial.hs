a5c5662162bf3f69261e1afe2687918149dad26e
Andre Santos <andre.santos@ifactory.com.br>
2017-09-11 17:57:02 -0300

    haskell tutorial

--- haskell/tutorial.hs
+++ haskell/tutorial.hs
@@ -8,3 +8,15 @@ doubleSmallNumber x =
     else x
 
 numbers = [1,2,3,4,5,6,7]
+
+r = 5.0
+
+-- subtract 12 from the half part of the supplied arg --
+
+subtractByTwelve a = a/2 - 12
+
+-- volume of a box --
+
+squareVol x y = x * y
+
+boxVol x y h = (squareVol x y) * h
